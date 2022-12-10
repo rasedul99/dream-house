cc5acead96540204346cb3fb63b224655836c359
rasedul99 <rasedul2199@gmail.com>
2022-12-10 20:14:12 +0600

    Header & home page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+// import pages
+import Home from "./pages/Home";
+import PropertyDetails from "./pages/PropertyDetails";
 
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <div className="max-w-[1440px] mx-auto bg-white">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
